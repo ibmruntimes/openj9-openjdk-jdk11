612cc4006afb6af25fd3e953af9f578e13e251f5
Andrew Leonard <andrew_m_leonard@uk.ibm.com>
2018-02-21 16:16:03 +0000

    Remove IBM Copyright
    Signed-off-by: Andrew Leonard <andrew_m_leonard@uk.ibm.com>

--- src/jdk.management.agent/share/classes/sun/management/jdp/JdpController.java
+++ src/jdk.management.agent/share/classes/sun/management/jdp/JdpController.java
@@ -1,10 +1,4 @@
 /*
- * ===========================================================================
- * (c) Copyright IBM Corp. 2013, 2017 All Rights Reserved
- * ===========================================================================
- */
-
-/*
  * Copyright (c) 2013, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
