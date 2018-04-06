05b53611aea4372a5158770e5ee4dc7505c231ba
Peter Bain <peter_bain@ca.ibm.com>
2018-04-06 15:18:55 -0400

    Fix race condition in jdwp
    Normally, the event helper thread suspends all threads, then the debug loop in
    the listener thread receives a command to resume. The debugger may deadlock if
    the debug loop in the listener thread starts processing commands (e.g. resume
    threads) before the event helper completes the initialization (and suspends
    threads).
    
    This patch adds synchronization to ensure the event helper completes the
    initialization sequence before debugger commands are processed.
    
    Signed-off-by: Peter Bain <peter_bain@ca.ibm.com>

--- src/jdk.jdwp.agent/share/native/libjdwp/debugLoop.c
+++ src/jdk.jdwp.agent/share/native/libjdwp/debugLoop.c
@@ -1,5 +1,7 @@
-/*
- * Copyright (c) 1998, 2017, Oracle and/or its affiliates. All rights reserved.
+/*******************************************************************************
+ * (c) Copyright IBM Corp. 2018, 2018 All Rights Reserved
+ *
+ * Copyright (c) 1998, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -98,7 +100,8 @@ debugLoop_run(void)
     standardHandlers_onConnect();
     threadControl_onConnect();
 
-    /* Okay, start reading cmds! */
+    debugInit_waitVMInitComplete();
+	/* Okay, start reading cmds! */
     while (shouldListen) {
         if (!dequeue(&p)) {
             break;
