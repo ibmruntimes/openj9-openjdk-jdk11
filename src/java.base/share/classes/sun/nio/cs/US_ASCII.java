b70a74fb8d665fb85b3415d163c29532a5df96f9
Steve Groeger <GROEGES@uk.ibm.com>
2018-10-29 10:08:12 +0000

    Updates to ensure correct copyright order
    Signed-off-by: Steve Groeger <GROEGES@uk.ibm.com>

--- src/java.base/share/classes/sun/nio/cs/US_ASCII.java
+++ src/java.base/share/classes/sun/nio/cs/US_ASCII.java
@@ -1,10 +1,4 @@
 /*
- * ===========================================================================
- * (c) Copyright IBM Corp. 2000, 2017 All Rights Reserved
- * ===========================================================================
- */
-
-/*
  * Copyright (c) 2000, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
@@ -29,6 +23,11 @@
  * questions.
  */
 
+/*
+ * ===========================================================================
+ * (c) Copyright IBM Corp. 2018, 2018 All Rights Reserved
+ * ===========================================================================
+ */
 package sun.nio.cs;
 
 import java.nio.ByteBuffer;
