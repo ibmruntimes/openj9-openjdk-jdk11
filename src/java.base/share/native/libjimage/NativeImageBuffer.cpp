a9519d3a6a53c33769da9bd29bb8977abc4a964f
Adam Farley <adam.farley@uk.ibm.com>
2018-11-05 16:24:49 +0000

    License adjustment
    Undoing a recent change to a custom license, to bring it back in line
    with expected formatting.
    
    Signed-off-by: Adam Farley <adam.farley@uk.ibm.com>

--- src/java.base/share/native/libjimage/NativeImageBuffer.cpp
+++ src/java.base/share/native/libjimage/NativeImageBuffer.cpp
@@ -1,4 +1,6 @@
 /*
+ * Copyright (c) 2015, 2016, Oracle and/or its affiliates. All rights reserved.
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
