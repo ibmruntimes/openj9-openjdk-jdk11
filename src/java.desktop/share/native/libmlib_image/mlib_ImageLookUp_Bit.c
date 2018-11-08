1d393aaf15d0dc3b91523f3cdbbbf3dd17b849ad
Adam Farley <adam.farley@uk.ibm.com>
2018-11-08 11:10:14 +0000

    Removing IBM License
    An IBM License is no longer required for this file. Also, the
    modification to the Oracle date is not needed, so that's being undone as
    well.
    
    Signed-off-by: Adam Farley <adam.farley@uk.ibm.com>

--- src/java.desktop/share/native/libmlib_image/mlib_ImageLookUp_Bit.c
+++ src/java.desktop/share/native/libmlib_image/mlib_ImageLookUp_Bit.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,11 +22,7 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-/*
- * ===========================================================================
- * (c) Copyright IBM Corp. 2018, 2018 All Rights Reserved
- * ===========================================================================
- */
+
 
 /*
  * FUNCTION
