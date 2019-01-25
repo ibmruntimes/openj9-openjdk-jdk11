19e6adb20fa9d225162b7f00d7cc8620aee4a940
Jerry Lui <jerrylui803@gmail.com>
2019-01-25 19:45:28 +0000

    fix NativeCrypto JNI being publicly accessible
    Signed-off-by: Jerry Lui <jerrylui@ibm.com>

--- closed/src/java.base/share/classes/jdk/crypto/jniprovider/NativeCrypto.java
+++ closed/src/java.base/share/classes/jdk/crypto/jniprovider/NativeCrypto.java
@@ -26,6 +26,12 @@ package jdk.crypto.jniprovider;
 
 import java.security.*;
 
+import com.ibm.oti.vm.VM;
+
+import jdk.internal.misc.Unsafe;
+import jdk.internal.reflect.Reflection;
+import jdk.internal.reflect.CallerSensitive;
+
 public class NativeCrypto {
 
     private static boolean loaded = false;
@@ -48,115 +54,130 @@ public class NativeCrypto {
         return loaded;
     }
 
+    private NativeCrypto() {
+        //empty
+    }
+
+    @CallerSensitive
+    public static NativeCrypto getNativeCrypto() {
+
+        ClassLoader callerClassLoader = Reflection.getCallerClass().getClassLoader();
+
+        if ((callerClassLoader != null) && (callerClassLoader != VM.getVMLangAccess().getExtClassLoader())) {
+            throw new SecurityException("NativeCrypto");
+        }
+        return new NativeCrypto();
+    }
+
     /* Native digest interfaces */
-    public static final native long DigestCreateContext(long nativeBuffer,
-                                                        int algoIndex);
+    public final native long DigestCreateContext(long nativeBuffer,
+                                                 int algoIndex);
 
-    public static final native int DigestDestroyContext(long context);
+    public final native int DigestDestroyContext(long context);
 
-    public static final native int DigestUpdate(long context,
-                                                byte[] message,
-                                                int messageOffset,
-                                                int messageLen);
+    public final native int DigestUpdate(long context,
+                                         byte[] message,
+                                         int messageOffset,
+                                         int messageLen);
 
-    public static final native int DigestComputeAndReset(long context,
-                                                         byte[] message,
-                                                         int messageOffset,
-                                                         int messageLen,
-                                                         byte[] digest,
-                                                         int digestOffset,
-                                                         int digestLen);
+    public final native int DigestComputeAndReset(long context,
+                                                  byte[] message,
+                                                  int messageOffset,
+                                                  int messageLen,
+                                                  byte[] digest,
+                                                  int digestOffset,
+                                                  int digestLen);
 
-    public static final native void DigestReset(long context);
+    public final native void DigestReset(long context);
 
     /* Native CBC interfaces */
-    public static final native long CBCCreateContext(long nativeBuffer,
-                                                     long nativeBuffer2);
+    public final native long CBCCreateContext(long nativeBuffer,
+                                              long nativeBuffer2);
 
-    public static final native int CBCDestroyContext(long context);
+    public final native int CBCDestroyContext(long context);
 
-    public static final native int CBCInit(long context,
-                                            int mode,
-                                            byte[] iv,
-                                            int ivlen,
-                                            byte[] key,
-                                            int keylen);
+    public final native int CBCInit(long context,
+                                    int mode,
+                                    byte[] iv,
+                                    int ivlen,
+                                    byte[] key,
+                                    int keylen);
 
-    public static final native int  CBCUpdate(long context,
-                                              byte[] input,
-                                              int inputOffset,
-                                              int inputLen,
-                                              byte[] output,
-                                              int outputOffset);
+    public final native int  CBCUpdate(long context,
+                                       byte[] input,
+                                       int inputOffset,
+                                       int inputLen,
+                                       byte[] output,
+                                       int outputOffset);
 
-    public static final native int  CBCFinalEncrypt(long context,
-                                               byte[] input,
-                                               int inputOffset,
-                                               int inputLen,
-                                               byte[] output,
-                                               int outputOffset);
+    public final native int  CBCFinalEncrypt(long context,
+                                             byte[] input,
+                                             int inputOffset,
+                                             int inputLen,
+                                             byte[] output,
+                                             int outputOffset);
 
     /* Native GCM interfaces */
-    public static final native int GCMEncrypt(byte[] key,
-                                              int keylen,
-                                              byte[] iv,
-                                              int ivlen,
-                                              byte[] input,
-                                              int inOffset,
-                                              int inLen,
-                                              byte[] output,
-                                              int outOffset,
-                                              byte[] aad,
-                                              int aadLen,
-                                              int tagLen);
+    public final native int GCMEncrypt(byte[] key,
+                                       int keylen,
+                                       byte[] iv,
+                                       int ivlen,
+                                       byte[] input,
+                                       int inOffset,
+                                       int inLen,
+                                       byte[] output,
+                                       int outOffset,
+                                       byte[] aad,
+                                       int aadLen,
+                                       int tagLen);
 
-    public static final native int GCMDecrypt(byte[] key,
-                                              int keylen,
-                                              byte[] iv,
-                                              int ivlen,
-                                              byte[] input,
-                                              int inOffset,
-                                              int inLen,
-                                              byte[] output,
-                                              int outOffset,
-                                              byte[] aad,
-                                              int aadLen,
-                                              int tagLen);
+    public final native int GCMDecrypt(byte[] key,
+                                       int keylen,
+                                       byte[] iv,
+                                       int ivlen,
+                                       byte[] input,
+                                       int inOffset,
+                                       int inLen,
+                                       byte[] output,
+                                       int outOffset,
+                                       byte[] aad,
+                                       int aadLen,
+                                       int tagLen);
 
     /* Native RSA interfaces */
-    public static final native long createRSAPublicKey(byte[] n,
-                                                       int nLen,
-                                                       byte[] e,
-                                                       int eLen);
+    public final native long createRSAPublicKey(byte[] n,
+                                                int nLen,
+                                                byte[] e,
+                                                int eLen);
 
-    public static final native long createRSAPrivateCrtKey(byte[] n,
-                                                           int nLen,
-                                                           byte[] d,
-                                                           int dLen,
-                                                           byte[] e,
-                                                           int eLen,
-                                                           byte[] p,
-                                                           int pLen,
-                                                           byte[] q,
-                                                           int qLen,
-                                                           byte[] dp,
-                                                           int dpLen,
-                                                           byte[] dq,
-                                                           int dqLen,
-                                                           byte[] qinv,
-                                                           int qinvLen);
+    public final native long createRSAPrivateCrtKey(byte[] n,
+                                                    int nLen,
+                                                    byte[] d,
+                                                    int dLen,
+                                                    byte[] e,
+                                                    int eLen,
+                                                    byte[] p,
+                                                    int pLen,
+                                                    byte[] q,
+                                                    int qLen,
+                                                    byte[] dp,
+                                                    int dpLen,
+                                                    byte[] dq,
+                                                    int dqLen,
+                                                    byte[] qinv,
+                                                    int qinvLen);
 
-    public static final native void destroyRSAKey(long key);
+    public final native void destroyRSAKey(long key);
 
-    public static final native int RSADP(byte[] k,
-                                         int kLen,
-                                         byte[] m,
-                                         int verify,
-                                         long RSAPrivateCrtKey);
+    public final native int RSADP(byte[] k,
+                                  int kLen,
+                                  byte[] m,
+                                  int verify,
+                                  long RSAPrivateCrtKey);
 
-    public static final native int RSAEP(byte[] k,
-                                         int kLen,
-                                         byte[] m,
-                                         long RSAPublicKey);
+    public final native int RSAEP(byte[] k,
+                                  int kLen,
+                                  byte[] m,
+                                  long RSAPublicKey);
 
 }
