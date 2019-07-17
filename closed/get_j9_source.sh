fa49279450d1ea339b11753cd601af20cf9ca37a
andrew-m-leonard <andrew_m_leonard@uk.ibm.com>
2019-07-17 20:07:40 +0100

    Update get_j9_source.sh to openj9-0.15.1
    Signed-off-by: andrew-m-leonard <andrew_m_leonard@uk.ibm.com>

--- closed/get_j9_source.sh
+++ closed/get_j9_source.sh
@@ -54,10 +54,10 @@ declare -A git_urls
 declare -A shas
 
 git_urls[openj9]=https://github.com/eclipse/openj9
-branches[openj9]=v0.15.0-release
+branches[openj9]=openj9-0.15.1
 
 git_urls[omr]=https://github.com/eclipse/openj9-omr
-branches[omr]=v0.15.0-release
+branches[omr]=openj9-0.15.1
 
 pflag=false
 
