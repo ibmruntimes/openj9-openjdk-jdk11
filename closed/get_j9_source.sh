3e61f7298a0843284068edd0c630a69e3b71efaf
andrew-m-leonard <31470007+andrew-m-leonard@users.noreply.github.com>
2020-01-09 11:57:08 +0000

    Update to openj9-0.18.0-m2 (#246)
    Signed-off-by: andrew-m-leonard <andrew_m_leonard@uk.ibm.com>

--- closed/get_j9_source.sh
+++ closed/get_j9_source.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ===========================================================================
-# (c) Copyright IBM Corp. 2017, 2019 All Rights Reserved
+# (c) Copyright IBM Corp. 2017, 2020 All Rights Reserved
 # ===========================================================================
 #
 # This code is free software; you can redistribute it and/or modify it
@@ -54,10 +54,10 @@ declare -A git_urls
 declare -A shas
 
 git_urls[openj9]=https://github.com/eclipse/openj9
-branches[openj9]=openj9-0.18.0-m1
+branches[openj9]=openj9-0.18.0-m2
 
 git_urls[omr]=https://github.com/eclipse/openj9-omr
-branches[omr]=openj9-0.18.0-m1
+branches[omr]=openj9-0.18.0-m2
 
 pflag=false
 
