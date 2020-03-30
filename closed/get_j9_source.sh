732d5f7a8ebe4e69157182f7ecb629cd100f898c
Simon Rushton <rushton@uk.ibm.com>
2020-03-30 12:34:35 +0100

    Update openj9-0.20.0 branch to fetch openj9-0.20.0-m2
    Signed-off-by: Simon Rushton <rushton@uk.ibm.com>

--- closed/get_j9_source.sh
+++ closed/get_j9_source.sh
@@ -54,10 +54,10 @@ declare -A git_urls
 declare -A shas
 
 git_urls[openj9]=https://github.com/eclipse/openj9
-branches[openj9]=openj9-0.20.0-m1
+branches[openj9]=openj9-0.20.0-m2
 
 git_urls[omr]=https://github.com/eclipse/openj9-omr
-branches[omr]=openj9-0.20.0-m1
+branches[omr]=openj9-0.20.0-m2
 
 pflag=false
 
