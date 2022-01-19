3e0def07d2ffc2a24f867d8fd3863775dfff939d
Peter Shipton <Peter_Shipton@ca.ibm.com>
2022-01-18 21:56:55 -0500

    Update versions to build openj9-0.30.0-m3a
    Signed-off-by: Peter Shipton <Peter_Shipton@ca.ibm.com>

--- closed/get_j9_source.sh
+++ closed/get_j9_source.sh
@@ -57,7 +57,7 @@ declare -A shas
 declare -A references
 
 git_urls[openj9]=https://github.com/eclipse-openj9/openj9
-branches[openj9]=openj9-0.30.0-m3
+branches[openj9]=openj9-0.30.0-m3a
 
 git_urls[omr]=https://github.com/eclipse-openj9/openj9-omr
 branches[omr]=openj9-0.30.0-m3
