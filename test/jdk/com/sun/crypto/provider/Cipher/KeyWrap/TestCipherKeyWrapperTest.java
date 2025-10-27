323f0dd0fae853c69e7df3a2d2ffae5df302ec56
Tom Ginader <thomas.ginader@ibm.com>
2025-10-27 14:58:44 -0400

    Add provider check for Cipher unwrapping operation
    In OpenJCEPlus, engineInit throws an error when AlgorithmParameters
    is not null. This update changes the else-if logic so it will pass
    null params to engineInit in the provider OpenJCEPlus/OpenJCEPlusFIPS.
    
    Signed-off-by: Tom Ginader <thomas.ginader@ibm.com>

--- test/jdk/com/sun/crypto/provider/Cipher/KeyWrap/TestCipherKeyWrapperTest.java
+++ test/jdk/com/sun/crypto/provider/Cipher/KeyWrap/TestCipherKeyWrapperTest.java
@@ -21,6 +21,12 @@
  * questions.
  */
 
+/*
+ * ===========================================================================
+ * (c) Copyright IBM Corp. 2025, 2025 All Rights Reserved
+ * ===========================================================================
+ */
+
 import static java.lang.System.out;
 
 import java.lang.Integer;
@@ -291,7 +297,7 @@ public class TestCipherKeyWrapperTest {
         Cipher wrapCI = Cipher.getInstance(wrapAlgo);
         if (isPBE && !isAESBlowfish) {
             wrapCI.init(Cipher.WRAP_MODE, initKey, pbeParams);
-        } else if (isAESBlowfish) {
+        } else if (isAESBlowfish && !wrapCI.getProvider().getName().startsWith("OpenJCEPlus")) {
             wrapCI.init(Cipher.WRAP_MODE, initKey);
             aps = wrapCI.getParameters();
         } else {
